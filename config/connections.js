0f26c3cd335ba49c022dba48328042a557482cb0
Isabel M. Arias Salas <isabelminerva@outlook.com>
2017-11-16 09:09:22 -0400

    Conexion a Jaws

--- config/connections.js
+++ config/connections.js
@@ -40,6 +40,22 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "db_name",
+    });
+};  
+
+
   mysql: {
     adapter    : 'sails-mysql',
     host      : 'localhost',
